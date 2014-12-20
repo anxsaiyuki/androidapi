fca9592e407da64d41d22eaf59e4825eca7aa862
anxsaiyuki <anxsaiyuki@gmail.com>
2014-12-20 17:35:51 +0800

    syyntax

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ActionController::Base
         p params
         p "=================================================="
         
-        @cards = Card.where("card_type = UNIT")
+        @cards = Card.where("card_type", "UNIT")
         p "===================================================="
         p @cards
         p "=================================================="
