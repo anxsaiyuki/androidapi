bb7e19236fa1b8d98a556fefa624ad4bedb6f336
anxsaiyuki <anxsaiyuki@gmail.com>
2014-12-27 11:07:29 +0800

    test sahre

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -129,9 +129,8 @@ class ApiController < ActionController::Base
 
             render json: {data: @deckName.to_a}, status: 200
         elsif params[:deck_action] == "share"
-            @deckName = DeckName.select("deck_name, user_id")
-            @deckName = @deckName.where(share_user_id: 1)
-            @deckName = @deckName.where.not(user_id: params[:user_id])
+            @deckName = DeckName.joins(:share_deck).select("deck_name, user_id")
+            @deckName = @deckName.where(share_user_id: params[:user_id])
             
             render json: {data: @deckName.to_a}, status: 200
         end
