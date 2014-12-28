40d1dd66ef249e01d0748dce6a4af519432800e7
anxsaiyuki <anxsaiyuki@gmail.com>
2014-12-28 22:38:00 +0800

    AsdF

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -139,7 +139,7 @@ class ApiController < ActionController::Base
 
             render json: {data: @deckName.to_a}, status: 200
         elsif params[:deck_action] == "share"
-            @deckName = ShareDeck.joins("LEFT JOIN deck_names ON deck_names.id = share_decks.deck_name_id").where(share_user_id: params[:user_id])
+            @deckName = ShareDeck.select("share_decks.user_id, share_decks.share_user_id, deck_names.deck_name").joins("LEFT JOIN deck_names ON deck_names.id = share_decks.deck_name_id").where(share_user_id: params[:user_id])
             
             render json: {data: @deckName.to_a}, status: 200
         end
