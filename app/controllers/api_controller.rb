50497f9c2e9c5d253c50a67938e71ba698865f9c
anxsaiyuki <anxsaiyuki@gmail.com>
2015-02-10 15:23:14 +0800

    deck_summary

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -209,7 +209,7 @@ class ApiController < ActionController::Base
     
     def getdeck
         if params[:deck_action] == "own"
-            @deckName = DeckName.select("id, deck_name, user_id")
+            @deckName = DeckName.select("id, deck_name, deck_summary, user_id")
             @deckName = @deckName.where(user_id: params[:user_id])
 
             render json: {data: @deckName.to_a}, status: 200
