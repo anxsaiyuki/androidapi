ba7dbe5bee08cb161a170fb50cce7f87cef3380d
anxsaiyuki <anxsaiyuki@gmail.com>
2015-02-15 14:06:54 +0800

    comment

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -286,15 +286,17 @@ class ApiController < ActionController::Base
         if params[:deck_comment_action] == "create"
             @deckComment = DeckComment.create(deck_id: params[:deck_id], user_id: params[:user_id], comment: params[:deck_comment].to_s, priority: 0)
             render json: {message: '1'}, status: 200
-        
-        elsif params[:deck_comment_action] == "get"
-            @deckComment = DeckComment.select("deck_comments.id, users.user_name, deck_comments.comment, deck_comments.priority").joins("LEFT JOIN users ON users.id = deck_comments.user_id").where(:deck_comments => {:deck_id => params[:deck_id]})
+        elsif params[:deck_comment_action] == "add_priority"
             
-            render json: {data: @deckComment}, status: 200
+        elsif params[:deck_comment_action] == "get"
+            @deckComment = DeckComment.select("deck_comments.id, users.user_name, users.id, deck_comments.comment, deck_comments.priority").joins("LEFT JOIN users ON users.id = deck_comments.user_id").where(:deck_comments => {:deck_id => params[:deck_id]})
+            @deckOwner = DeckName.find_by_id(params[:deck_id])
+            render json: {data: @deckComment, sub_data: @deckOwner}, status: 200
         elsif params[:deck_comment_action] == "delete"
             deckComment = DeckComment.find(params[:deck_comment_id])
 			deckComment.destroy
             render json: {message: '1'}, status: 200
+        
         end
     end
 
